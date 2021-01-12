8e53cb993269de9a9a81c0bf14eb5f63fd1db810
isaccanedo <isaccanedo@gmail.com>
2021-01-12 10:53:18 -0300

    :shirt: Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Main {
+
+	/*
+	 * What will this program print out to the console when executed? (O que este
+	 * programa imprimirá no console quando executado?)
+	 * 
+	 * 1 - [10, 1, 5]
+	 * 2 - [1, 5, 10]
+	 * 3 - [10, 5, 1]
+	 * 4 - [5, 1, 10] ==> correct
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(5);
+		list.add(1);
+		list.add(10);
+
+		System.out.println(list);
+
+	}
+
+}
